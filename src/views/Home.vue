17b25e54767713cc270af97f5ceb298f0d86a0a1
Ubong Jacob <jacobubong12@gmail.com>
2022-03-12 22:00:49 +0100

    added dataTitle component and functionality

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,46 @@
 <template>
-  <div class="home">Hello world</div>
+  <main v-if="!loading">
+    <DataTitle :text="title" :dataDate="dataDate" />
+  </main>
+
+  <main v-else class="flex flex-col align-center justify-center text-center">
+    <div class="text-gray-500 text 3xl mt-10 mb-6">fetching data</div>
+    <img :src="loadingImage" class="w-24 m-auto" alt="loading data" />
+  </main>
 </template>
 
 <script>
+import DataTitle from '@/components/DataTitle';
+
 export default {
   name: 'Home',
-  components: {},
-  created() {},
+  data() {
+    return {
+      loading: true,
+      title: 'Global',
+      dataDate: '',
+      stats: {},
+      countries: [],
+      loadingImage: require('../assets/hourglass.gif'),
+    };
+  },
+  components: {
+    DataTitle,
+  },
+  async created() {
+    const data = await this.fetchCovidData();
+    this.dataDate = data.Date;
+    this.stats = data.Global;
+    this.countries = data.Countries;
+    this.loading = false;
+  },
+  methods: {
+    async fetchCovidData() {
+      const res = await fetch('https://api.covid19api.com/summary');
+      const data = await res.json();
+      console.log(data);
+      return data;
+    },
+  },
 };
 </script>
