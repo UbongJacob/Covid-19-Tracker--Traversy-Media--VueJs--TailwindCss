0edfef3700f517383606e0a1a9fcef86dc3da1fe
Ubong Jacob <jacobubong12@gmail.com>
2022-03-12 19:11:26 +0100

    Implemented the header component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,12 @@
 <template>
-  <header>
-    <div>Covid-19 Tracker</div>
+  <header class="text-center bg-blue-800 text-white p-4 mb-10">
+    <div class="text-3xl md:text-5xl font-bold mb-3">
+      <i class="fa fa-viruses"></i> Covid-19 Tracker
+    </div>
+    <p>
+      API by
+      <a href="https://covid19api.com" target="_blank"><u>covid19api.com</u></a>
+    </p>
   </header>
 </template>
 
